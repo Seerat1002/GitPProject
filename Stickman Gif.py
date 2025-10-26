9560c4ea455ac0723a1145e89d88996f1f5a1199
Seerat <seeratkaur.one@gmail.com>
2025-10-26 11:45:30 +0530

    Added comment for stickman gif compilation
    Added a comment to describe the purpose of the script.

--- Stickman Gif.py
+++ Stickman Gif.py
@@ -1,3 +1,5 @@
+#Compilation of images to create a stickman dancing gif
+
 import imageio.v3 as iio
 
 filenames = ['Stickman1.jpg', 'Stickman2.jpg', 'Stickman3.jpg', 'Stickman4.jpg', 'Stickman5.jpg', 'Stickman6.jpg', 'Stickman7.jpg', 'Stickman8.jpg']
