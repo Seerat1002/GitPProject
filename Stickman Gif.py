7576cdd588400d29870c4a013b38df4b0e629f6c
Seerat <seeratkaur.one@gmail.com>
2025-10-26 11:29:44 +0530

    Stickman gif

---
+++ Stickman Gif.py
@@ -0,0 +1,9 @@
+import imageio.v3 as iio
+
+filenames = ['Stickman1.jpg', 'Stickman2.jpg', 'Stickman3.jpg', 'Stickman4.jpg', 'Stickman5.jpg', 'Stickman6.jpg', 'Stickman7.jpg', 'Stickman8.jpg']
+images = []
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('Stickmangif.gif', images, duration = 500, loop = 0)
